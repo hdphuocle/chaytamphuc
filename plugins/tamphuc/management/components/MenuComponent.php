a675bf7b5d02b324919073ba1efd308d3abb7dae
Phuoc Le <phuoc.le@hdwebsoft.co>
2021-01-03 23:17:33 +0700

    hp

--- plugins/tamphuc/management/components/MenuComponent.php
+++ plugins/tamphuc/management/components/MenuComponent.php
@@ -1,8 +1,10 @@
 <?php namespace TamPhuc\Management\Components;
 
 use Cms\Classes\ComponentBase;
+use Illuminate\Support\Facades\DB;
 use TamPhuc\Management\Models\Menu;
 use TamPhuc\Management\Models\Type;
+use TamPhuc\Management\Models\Location;
 
 class MenuComponent extends ComponentBase
 {
@@ -17,8 +19,43 @@ class MenuComponent extends ComponentBase
     public $menuList;
     public $typeList;
 
+    function getUserIP()
+    {
+        $client = @$_SERVER['HTTP_CLIENT_IP'];
+        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+        $remote = $_SERVER['REMOTE_ADDR'];
+
+        if (filter_var($client, FILTER_VALIDATE_IP)) {
+            $ip = $client;
+        } elseif (filter_var($forward, FILTER_VALIDATE_IP)) {
+            $ip = $forward;
+        } else {
+            $ip = $remote;
+        }
+        return $ip;
+    }
+
     public function onRun()
     {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip_address = $_SERVER['REMOTE_ADDR'];
+        }
+        echo $ip_address;
+
+
+        $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$ip_address"));
+        $country = $geo['geoplugin_countryName'];
+        $city = $geo["geoplugin_city"];
+        echo $city;
+        if ($city) {
+            $location = Db::select("select * from tamphuc_management_location where LIKE '%$city%'", [1]);
+            echo $location;
+        }
+
         $this->menuList = Menu::all();
 
         $this->typeList = Type::orderBy('position')->get();
--- plugins/tamphuc/management/components/ServiceComponent.php
+++ plugins/tamphuc/management/components/ServiceComponent.php
@@ -15,48 +15,9 @@ class ServiceComponent extends ComponentBase
 
     public $serviceList;
 
-    function getUserIP()
-    {
-        $client = @$_SERVER['HTTP_CLIENT_IP'];
-        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-        $remote = $_SERVER['REMOTE_ADDR'];
-
-        if (filter_var($client, FILTER_VALIDATE_IP)) {
-            $ip = $client;
-        } elseif (filter_var($forward, FILTER_VALIDATE_IP)) {
-            $ip = $forward;
-        } else {
-            $ip = $remote;
-        }
-
-        return $ip;
-    }
-
-
     public function onRun()
     {
         $this->serviceList = Service::orderBy('position')->get();
-        if (!empty($_SERVER['HTTP_CLIENT_IP']))
-        {
-            $ip_address = $_SERVER['HTTP_CLIENT_IP'];
-        }
-//whether ip is from proxy
-        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-        {
-            $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        }
-//whether ip is from remote address
-        else
-        {
-            $ip_address = $_SERVER['REMOTE_ADDR'];
-        }
-        echo $ip_address;
-
-        $user_ip = '14.161.40.167';
-        $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$ip_address"));
-        $country = $geo['geoplugin_countryName'];
-        $city = $geo["geoplugin_city"];
-        echo $city;
     }
 
     public function defineProperties()
