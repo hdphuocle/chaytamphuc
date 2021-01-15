a1b8583890f4f0454f4612e5495cae87398003a0
Phuoc Le <phuoc.le@hdwebsoft.co>
2021-01-15 16:28:38 +0700

    Update content and deploy

--- themes/tamphuc/assets/js/main.js
+++ themes/tamphuc/assets/js/main.js
@@ -16,12 +16,13 @@ $(document).ready(function () {
     });
 
     $('.testimonails-slider').flexslider({
+        animation: "slide",
         prevText: '',
         nextText: '',
         controlNav: false
     });
 
-    $('#category-tabs li a').click(function(){
+    $('#category-tabs li a').click(function () {
         $(this).next('ul').slideToggle('500');
         $(this).find('i').toggleClass('fa-minus fa-plus ')
     });
@@ -29,16 +30,16 @@ $(document).ready(function () {
 
 
 });
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200)   // Fade in the arrow
     } else {
         $('#return-to-top').fadeOut(200); // Else fade out the arrow
     }
 });
-$('#return-to-top').click(function() {      // When arrow is clicked
+$('#return-to-top').click(function () {      // When arrow is clicked
     $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
+        scrollTop: 0                       // Scroll to top of body
     }, 500);
 });
 
