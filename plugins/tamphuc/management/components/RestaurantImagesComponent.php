1dd3ad02d528e1a5e3dfd6ee331bd8e07134a070
Phuoc Le <phuoc.le@hdwebsoft.co>
2021-01-17 11:19:48 +0700

    Update content and deploy

--- plugins/tamphuc/management/components/RestaurantImagesComponent.php
+++ plugins/tamphuc/management/components/RestaurantImagesComponent.php
@@ -9,14 +9,17 @@ class RestaurantImagesComponent extends ComponentBase
     public function componentDetails()
     {
         return [
-            'name'        => 'RestaurantImages Component',
+            'name' => 'RestaurantImages Component',
             'description' => 'No description provided yet...'
         ];
     }
+
     public $imageCategory;
 
     public function onRun()
     {
+//        $this->addJs('/plugins/tamphuc/management/assets/js/image-category.js', ['defer' => true]);
+
         $this->imageCategory = ImageCategory::orderBy('position')->get();
     }
 
