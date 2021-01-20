cbb7cfc7fcf594cd735391b4cf6fd7200746e7c6
Phuoc Le <phuoc.le@hdwebsoft.co>
2021-01-20 11:00:57 +0700

    Update content and deploy

--- themes/tamphuc/assets/js/main.js
+++ themes/tamphuc/assets/js/main.js
@@ -26,17 +26,18 @@ $(document).ready(function () {
         $(this).next('ul').slideToggle('500');
         $(this).find('i').toggleClass('fa-minus fa-plus ')
     });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200)   // Fade in the arrow
+        } else {
+            $('#return-to-top').fadeOut(200); // Else fade out the arrow
+        }
+    });
+    $('#return-to-top').click(function () {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop: 0                       // Scroll to top of body
+        }, 500);
+    });
 });
-$(window).scroll(function () {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200)   // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200); // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function () {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop: 0                       // Scroll to top of body
-    }, 500);
-});
+
 
